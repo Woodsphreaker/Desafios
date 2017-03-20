02ffeb111ca45c1bb4592a3638d6c5a225db95a4
Carlo Enrico <carlo.enrico@uol.com.br>
2017-03-20 01:06:33 -0300

    updade desafio ibmep

--- Ibmep 2ª Fase/desafio_obmep_v2.js
+++ Ibmep 2ª Fase/desafio_obmep_v2.js
@@ -7,21 +7,21 @@ const splitNumber = (number) => {
     return numbers;
 };
 const toNumber = (arr) => parseInt(arr.join(''));
-const reducer  = (arr) => arr.reduce((_a,_b) => _a + _b);
+const reducerSUM = (arr) => arr.reduce((_a, _b) => _a + _b);
 const operators = {
-    sum: (number, prop) => number.map((_a) => (_a + prop)),
-    sub: (number,prop) => number.map((_a) => (_a - prop)),
-    div: (number,prop) => number.map((_a) => (_a / prop)),
-    mul: (number,prop) => number.map((_a) => (_a * prop))
+    sum: (number, prop) => number + prop,
+    sub: (number, prop) => number - prop,
+    div: (number, prop) => number / prop,
+    mul: (number, prop) => number * prop
 };
 const solve = (number) => {
-    const algarism_1 = reducer(splitNumber(number))
+    const algarism_1 = reducerSUM(splitNumber(number))
     const sub = operators['sub']([number], algarism_1);
-    const algarism_2 = reducer(splitNumber(sub));
+    const algarism_2 = reducerSUM(splitNumber(sub));
     const sum = operators['sum']([algarism_2], 4);
     const mult = operators['mul'](sum, toNumber(splitNumber(sum)));
     const final = operators['mul'](mult, 3)
-    return toNumber(final);
+    return final;
 };
 
 console.log(solve(39))
